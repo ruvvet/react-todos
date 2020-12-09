7dd7a1d36cc51dee5a742316829b0e873c37e3ea
Jenny <finej01@gmail.com>
2020-12-09 14:35:41 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import ListItem from './components/ListItem'
-
-
-const toDoList = [{priority: 1, thing: "Shower"}, {priority: 2, thing:"brush teeth"}, {priority: 5, thing:"hello"}]
-
-const printToDo = toDoList.map((todo, index) =>{
-  return <li><ListItem priority={todo.priority} thing={todo.thing} key={index}/></li>
-})
+import React, {useState} from 'react';
 
 
 
 function App() {
+  const [toDoList, setList] = useState(
+
+    [
+    {priority: 1, thing: "Shower"}, {priority: 2, thing:"brush teeth"}, {priority: 5, thing:"hello"}
+  ]
+
+
+  );
+
+  const clearList =() =>{
+    const deletefirst = toDoList.splice(1,(toDoList.length-1))
+    setList(deletefirst)
+  }
+
+  const printToDo = toDoList.map((todo, index) =>{
+    return <li><ListItem priority={todo.priority} thing={todo.thing} key={index}/></li>
+  })
+
   return (
     <div class="newClass">
     <header>My List</header>
@@ -19,6 +31,8 @@ function App() {
       <ul>
      {printToDo}
       </ul>
+
+      <button onClick={clearList}>CLEAR ALL</button>
     </div>
   </div>
   );
